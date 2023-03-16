92700a99de367d32857ef5cfbdb0011388c624d8
logvashgod <logvash@gmail.com>
2023-03-16 14:54:00 +0100

    updated.added new features

--- logic.js
+++ logic.js
@@ -2,6 +2,7 @@
 const level = document.getElementById("level");
 const levelNum = document.getElementById("level-num");
 const xp = document.getElementById("xp");
+const progressBar = document.getElementById("progress-bar");
 //storage buttons
 const saveProgress = document.getElementById("save_progress");
 const loadProgres = document.getElementById("load_progress");
@@ -23,6 +24,9 @@ currentXP = 0;
 const storedCurrentXP = localStorage.getItem("currentXP");
 const storedMaxXPValue = localStorage.getItem("maxXPValue");
 const storedLevelNum = localStorage.getItem("levelNum");
+const storedQUEST_ADDED = localStorage.getItem("QUEST_ADDED");
+const storedQUEST_ADDED1 = localStorage.getItem("QUEST_ADDED1");
+const storedQUEST_ADDED2 = localStorage.getItem("QUEST_ADDED2");
 
 //buttons
 
@@ -50,6 +54,7 @@ button3.addEventListener("click", () => {
   xpNeed = maxXPValue.innerHTML;
   if (currentXP >= xpNeed) {
     updateLevel();
+    updateProgressBar();
   }
 });
 
@@ -67,16 +72,56 @@ function updateLevel() {
   levelNum.innerHTML++;
   let lvlInfo = levelNum.innerHTML;
   window.alert(`Now you reached ${lvlInfo++} lvl`);
-  congratsNum.innerHTML++;
 }
+//addtask
+
+const ADD_QUEST = document.getElementById("add_quest");
+const ADD_QUEST1 = document.getElementById("add_quest1");
+const ADD_QUEST2 = document.getElementById("add_quest2");
+const QUEST_DESCRIPTION = document.getElementById("1");
+const QUEST_ADDED = document.getElementById("quest1");
+const QUEST_ADDED1 = document.getElementById("quest2");
+const QUEST_ADDED2 = document.getElementById("quest3");
+const QUEST_TEXT = document.getElementById("quest_text");
+
+ADD_QUEST.addEventListener("click", () => {
+  const text = QUEST_DESCRIPTION.value;
+  QUEST_ADDED.innerHTML = text;
+});
+
+ADD_QUEST1.addEventListener("click", () => {
+  const text1 = QUEST_DESCRIPTION.value;
+  QUEST_ADDED1.innerHTML = text1;
+});
+
+ADD_QUEST2.addEventListener("click", () => {
+  const text2 = QUEST_DESCRIPTION.value;
+  QUEST_ADDED2.innerHTML = text2;
+});
+
+//deleting ask
+ADD_QUEST.addEventListener("dblclick", () => {
+  QUEST_ADDED.innerHTML = "";
+});
+
+ADD_QUEST1.addEventListener("dblclick", () => {
+  QUEST_ADDED1.innerHTML = "";
+});
+
+ADD_QUEST2.addEventListener("dblclick", () => {
+  QUEST_ADDED2.innerHTML = "";
+});
 
 //
 ///local storage
 
 saveProgress.addEventListener("click", () => {
-  localStorage.setItem("currentXP", currentXP);
+  localStorage.setItem("currentXP", minXPValue.innerHTML);
   localStorage.setItem("maxXPValue", maxXPValue.innerHTML);
   localStorage.setItem("levelNum", levelNum.innerHTML);
+  localStorage.setItem("QUEST_ADDED", QUEST_ADDED.innerHTML);
+  localStorage.setItem("QUEST_ADDED1", QUEST_ADDED1.innerHTML);
+  localStorage.setItem("QUEST_ADDED2", QUEST_ADDED2.innerHTML);
 });
 
 loadProgres.addEventListener("click", () => {
@@ -86,7 +131,100 @@ loadProgres.addEventListener("click", () => {
   // const storedLevelNum = localStorage.getItem("levelNum");
 
   // Set progress from local storage
-  currentXP.innerHTML = storedCurrentXP;
+  minXPValue.innerHTML = storedCurrentXP;
   maxXPValue.innerHTML = storedMaxXPValue;
   levelNum.innerHTML = storedLevelNum;
+  QUEST_ADDED.innerHTML = storedQUEST_ADDED;
+  QUEST_ADDED2.innerHTML = storedQUEST_ADDED1;
+  QUEST_ADDED2.innerHTML = storedQUEST_ADDED2;
 });
+
+window.addEventListener("load", () => {
+  minXPValue.innerHTML = storedCurrentXP;
+  maxXPValue.innerHTML = storedMaxXPValue;
+  levelNum.innerHTML = storedLevelNum;
+  QUEST_ADDED.innerHTML = storedQUEST_ADDED;
+  QUEST_ADDED2.innerHTML = storedQUEST_ADDED1;
+  QUEST_ADDED2.innerHTML = storedQUEST_ADDED2;
+});
+
+//--!--! task list
+const ITEMS_CONTAINER = document.getElementById("items");
+const ITEM_TEMPLATE = document.getElementById("itemTemplate");
+const ADD_BUTTON = document.getElementById("add");
+
+let items = getItems();
+//
+function getItems() {
+  const value = localStorage.getItem("todo") || "[]";
+
+  return JSON.parse(value);
+}
+//
+function setItems(items) {
+  const itemsJson = JSON.stringify(items);
+
+  localStorage.setItem("todo", itemsJson);
+}
+
+//adding items
+function addItems() {
+  items.unshift({
+    description: "",
+    completed: false,
+  });
+
+  setItems(items);
+  refreshList();
+}
+
+function updateItem(item, key, value) {
+  item[key] = value;
+
+  setItems(items);
+  refreshList();
+}
+
+function refreshList() {
+  //TODO:sort items
+  items.sort((a, b) => {
+    if (a.completed) {
+      return 1;
+    }
+
+    if (b.completed) {
+      return -1;
+    }
+
+    return a.description < b.description ? -1 : 1;
+  });
+
+  ITEMS_CONTAINER.innerHTML = "";
+
+  for (const item of items) {
+    const itemElement = ITEM_TEMPLATE.content.cloneNode(true);
+    const descriptionInput = itemElement.querySelector(".item-description");
+    const completedInput = itemElement.querySelector(".item-completed");
+
+    descriptionInput.value = item.description;
+    completedInput.checked = item.completed;
+
+    descriptionInput.addEventListener("change", () => {
+      updateItem(item, "description", descriptionInput.value);
+    });
+
+    completedInput.addEventListener("change", () => {
+      updateItem(item, "completed", completedInput.checked);
+    });
+
+    ITEMS_CONTAINER.append(itemElement);
+  }
+}
+
+ADD_BUTTON.addEventListener("click", () => {
+  addItems();
+});
+// refresh
+refreshList();
+
+////
--- skills.js
+++ skills.js
@@ -1,43 +1,23 @@
-const plus = document.getElementById("new_plus");
-const skillLvl = document.getElementById("skill_lvl");
-const skillExp = document.getElementById("xp_value");
-const xpNeed = document.getElementById("xpNeed");
-//s
-const plus1 = document.getElementById("new_plus1");
-const skillLvl1 = document.getElementById("skill_lvl1");
-const skillExp1 = document.getElementById("xp_value1");
-const xpNeed1 = document.getElementById("xpNeed1");
+const skillList = document.getElementById("skil-list");
+const addSkillBtn = document.getElementById("add-btn");
+const removeSkillBtn = document.getElementById("remove-btn");
+
+//array
+const mySkills = [];
+
 //
-plus.addEventListener("click", () => {
-  console.log("dadaya");
-  currentXp += 3;
-  skillExp.innerHTML = currentXp;
-  youNeedXP = xpNeed.innerHTML;
-  if (currentXp >= youNeedXP) {
-    console.log("da");
-    updateSkillLvl();
-  }
+mySkills.forEach((skill) => {
+  const li = document.createElement("li");
+  li.innerText = skill;
+  skillList.appendChild(li);
 });
 
-plus1.addEventListener("click", () => {
-  console.log("dadaya");
-  currentXp += 3;
-  skillExp.innerHTML = currentXp;
-  youNeedXP = xpNeed.innerHTML;
-  if (currentXp >= youNeedXP) {
-    console.log("da");
-    updateSkillLvl();
-  }
+//add skill
+addSkillBtn.addEventListener("click", () => {
+  const newSkill = prompt("Enter skill name: ");
+  mySkills.push(newSkill);
+  const li = document.createElement("li");
+  li.innerText = newSkill;
+  skillList.appendChild("li");
+  console.log("works");
 });
-
-currentXp = 0;
-
-function updateSkillLvl() {
-  currentXp = 0;
-  let youNeedXP = Number(xpNeed.innerHTML);
-  youNeedXP += 15;
-  xpNeed.innerHTML = youNeedXP;
-  skillLvl.innerHTML++;
-}
-
-///
